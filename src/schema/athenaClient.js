bef128bb2d9fb995d1dc6e8c24ca5260f30de3d3
Jonatan Sanchez Hinostroza <jsanchez25h@gmail.com>
2025-07-05 21:20:25 -0500

    Fix Service

--- src/schema/athenaClient.js
+++ src/schema/athenaClient.js
@@ -1,9 +1,26 @@
-import AWS from "aws-sdk";
+/*import AWS from "aws-sdk";
 import AthenaExpress from "athena-express";
 
 const athena = new AthenaExpress({
   aws: AWS,
   s3: process.env.ATHENA_OUTPUT_S3,
+  getStats: true
+});
+
+export default athena;
+*/
+
+import { AthenaClient } from "@aws-sdk/client-athena";
+import AthenaExpress     from "athena-express";
+
+const athenaClient = new AthenaClient({
+  region: process.env.AWS_REGION || "us-east-1"
+});
+
+const athena = new AthenaExpress({
+  athena  : athenaClient,                 // SDK v3 client
+  s3      : process.env.ATHENA_OUTPUT_S3, // "s3://bucket/path/"
+  db      : process.env.ATHENA_DATABASE,  // "my_database"
   getStats: true
 });
 
